52bbf0449e5de4af3ca060d1c64336a426d060f4
ericg-ignite <ericg-ignite@outlook.com>
2024-11-04 15:41:24 +0100

    initial code

---
+++ GetBlobContent/Program.cs
@@ -0,0 +1,45 @@
+
+
+using Azure.Storage.Blobs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+
+string key = "DefaultEndpointsProtocol=https;AccountName=ericgignite2024;AccountKey=x900cp99f+oxKCi6oc7qh7HTvHy2e8eFdIb7Ovyj3V0Tz7Tt9PmkGNKAX5qNQLQ8GLTRZFM88JYv+AStkFkYlQ==;EndpointSuffix=core.windows.net";
+string containerName = "container1";
+string blobName = "Ignite2024.txt";
+
+
+app.MapGet("/readBlobContent", async () =>
+    {
+        try
+        {
+            //create storage account client
+            var blobServiceClient = new BlobServiceClient(key);
+
+            //create container client
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            //create blob client
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            //download blob content
+            var response = await blobClient.DownloadAsync();
+
+            //read content
+            var content = await new StreamReader(response.Value.Content).ReadToEndAsync();
+            return content;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+
+    });
+
+
+app.Run();
+
+
